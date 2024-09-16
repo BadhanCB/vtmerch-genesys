07faf7dcbb16593122137f8b7ce1a47180b51e1b
BadhanCB <badhancsesastc112@gmail.com>
2024-09-16 11:43:05 +0600

    design section title

--- src/components/home/BestSelling/BestSellingTitle.jsx
+++ src/components/home/BestSelling/BestSellingTitle.jsx
@@ -1,7 +1,7 @@
 const BestSellingTitle = () => {
     return (
-        <h2 className="text-[4.375rem] uppercase font-semibold font-montserrat mb-8">
-            <span>Buy 1 get 1 free on</span> <span>best selling products</span>
+        <h2 className="text-[4.375rem] uppercase font-semibold font-montserrat mb-9 leading-tight">
+            Buy 1 get 1 free <br /> on <span>best selling products</span>
         </h2>
     );
 };
