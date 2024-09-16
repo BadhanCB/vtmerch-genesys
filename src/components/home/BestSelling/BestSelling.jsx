96e265c97e80b63fd67674701f0003fcd16109c0
BadhanCB <badhancsesastc112@gmail.com>
2024-09-16 13:04:35 +0600

    solved text stroke problem

--- src/components/home/BestSelling/BestSelling.jsx
+++ src/components/home/BestSelling/BestSelling.jsx
@@ -3,7 +3,7 @@ import BestSellingTitle from "./BestSellingTitle";
 
 const BestSelling = () => {
     return (
-        <section className="px-4 sm:px-12 md:px-16 lg:px-24 xl:px-28 2xl:px-[9.25rem] py-10 sm:py-12 md:py-16 lg:py-20 xl:py-24 2xl:py-[6.3125rem] bg-gray-100">
+        <section className="px-4 sm:px-12 md:px-16 lg:px-24 xl:px-28 2xl:px-[9.25rem] py-10 sm:py-12 md:py-16 lg:py-20 xl:py-24 2xl:py-[6.3125rem] bg-[#F6F5F5]">
             <BestSellingTitle />
             <BestSellingProductsList />
         </section>
--- src/components/home/BestSelling/BestSellingTitle.jsx
+++ src/components/home/BestSelling/BestSellingTitle.jsx
@@ -2,7 +2,7 @@ const BestSellingTitle = () => {
     return (
         <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[4.375rem] uppercase font-semibold font-montserrat mb-4 sm:mb-5 md:mb-6 lg:mb-7 xl:mb-8 2xl:mb-9 leading-tight">
             Buy 1 get 1 free <br /> on{" "}
-            <span className="stroke-text font-sans">best selling products</span>
+            <span className="stroke-text">best selling products</span>
         </h2>
     );
 };
