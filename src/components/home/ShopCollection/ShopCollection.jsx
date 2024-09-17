9a763665290df36c4c62dc376a9164c7685cc1dc
BadhanCB <badhancsesastc112@gmail.com>
2024-09-17 21:14:53 +0600

    designed shop collection section

--- src/components/home/ShopCollection/ShopCollection.jsx
+++ src/components/home/ShopCollection/ShopCollection.jsx
@@ -10,41 +10,41 @@ import ShopCollectionOptionCard from "./ShopCollectionOptionCard";
 
 const ShopCollection = () => {
     return (
-        <SectionLayout className="bg-gradient-to-b from-[#2C2A2A] to-black text-white font-montserrat h-[54.5625rem] grid grid-cols-12 grid-rows-12 gap-2 md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-12">
+        <SectionLayout className="bg-gradient-to-b from-[#2C2A2A] to-black text-white font-montserrat h-[54.5625rem] grid grid-cols-12 grid-rows-12 gap-2 md:gap-3 lg:gap-4 xl:gap-6 2xl:gap-8">
             <ShopCollectionHeader />
 
             <ShopCollectionOptionCard
-                className="bg-slate-400 col-start-1 col-end-7 row-start-3 row-end-7 md:col-start-9 md:col-end-13 md:row-start-1 md:row-end-7"
+                className="col-start-1 col-end-7 row-start-3 row-end-7 md:col-start-9 md:col-end-13 md:row-start-1 md:row-end-7"
                 title="The Future Looks Bright Collection"
                 image={futureLooksImg}
             />
 
             <ShopCollectionOptionCard
-                className="bg-green-700 col-start-7 col-end-13 row-start-3 row-end-7 md:col-start-1 md:col-end-5 lg:col-start-1 lg:col-end-4 md:row-start-5 md:row-end-11 lg:row-start-6 lg:row-end-13"
+                className="col-start-7 col-end-13 row-start-3 row-end-7 md:col-start-1 md:col-end-5 lg:col-start-1 lg:col-end-4 md:row-start-5 md:row-end-11 lg:row-start-6 lg:row-end-13"
                 title="THE BOSS COLLECTION"
                 image={bossCollectionImg}
             />
 
             <ShopCollectionOptionCard
-                className="bg-blue-400 col-start-1 col-end-9 row-start-9 row-end-11 md:col-start-5 md:col-end-9 lg:col-start-4 lg:col-end-7 md:row-start-4 md:row-end-8 lg:row-start-5 lg:row-end-9"
+                className="col-start-1 col-end-9 row-start-9 row-end-11 md:col-start-5 md:col-end-9 lg:col-start-4 lg:col-end-7 md:row-start-4 md:row-end-8 lg:row-start-5 lg:row-end-9"
                 title="HEADWEAR"
                 image={headWearImg}
             />
 
             <ShopCollectionOptionCard
-                className="bg-yellow-400 col-start-1 col-end-9 row-start-11 row-end-13 md:col-start-5 md:col-end-9 lg:col-start-4 lg:col-end-7 md:row-start-8 md:row-end-11 lg:row-start-9 lg:row-end-13"
+                className="col-start-1 col-end-9 row-start-11 row-end-13 md:col-start-5 md:col-end-9 lg:col-start-4 lg:col-end-7 md:row-start-8 md:row-end-11 lg:row-start-9 lg:row-end-13"
                 title="VALUE BOOKS"
                 image={valueBooksImg}
             />
 
             <ShopCollectionOptionCard
-                className="bg-pink-800 col-span-12 row-start-7 row-end-9 md:col-start-1 md:col-end-9 lg:col-start-7 lg:col-end-9 md:row-start-11 lg:row-start-3 md:row-end-13"
+                className="col-span-12 row-start-7 row-end-9 md:col-start-1 md:col-end-9 lg:col-start-7 lg:col-end-9 md:row-start-11 lg:row-start-3 md:row-end-13"
                 title="Accessories"
                 image={accessoriesImg}
             />
 
             <ShopCollectionOptionCard
-                className="bg-rose-400 col-start-9 col-end-13 row-start-9 row-end-13 md:col-start-9 md:col-end-13 md:row-start-7 md:row-end-13"
+                className="col-start-9 col-end-13 row-start-9 row-end-13 md:col-start-9 md:col-end-13 md:row-start-7 md:row-end-13"
                 title="TOPS"
                 image={topsImg}
             />
--- src/components/home/ShopCollection/ShopCollectionOptionCard.jsx
+++ src/components/home/ShopCollection/ShopCollectionOptionCard.jsx
@@ -1,7 +1,18 @@
+// eslint-disable-next-line react/prop-types
 const ShopCollectionOptionCard = ({ title, image, className }) => {
     return (
-        <div className={`transition-all duration-700 ${className}`}>
-            <h3>{title}</h3>
+        <div
+            className={`transition-all duration-700 relative overflow-hidden p-4 rounded-xl flex justify-center items-end ${className}`}
+        >
+            <h3 className="relative z-10 text-lg xl:text-2xl 2xl:text-3xl text-center font-semibold xl:font-bold">
+                {title}
+            </h3>
+            <img
+                src={image}
+                alt={title}
+                className="absolute top-0 left-0 h-full w-full object-cover z-[5]"
+            />
+            <div className="absolute top-0 left-0 h-full w-full bg-gradient-to-b from-[#7c7c7c] to-black opacity-40 z-[7]"></div>
         </div>
     );
 };
