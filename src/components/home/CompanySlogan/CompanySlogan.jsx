7e6093f5aca5ca9a02cdc8ebbe64e2593c065c54
BadhanCB <badhancsesastc112@gmail.com>
2024-09-18 21:18:42 +0600

    changed line hight for the text

--- src/components/home/CompanySlogan/CompanySlogan.jsx
+++ src/components/home/CompanySlogan/CompanySlogan.jsx
@@ -10,13 +10,15 @@ const CompanySlogan = () => {
                 alt="quote icon"
                 className="w-12 md:w-16 lg:w-32 xl:w-40 2xl:w-[13.8125rem] h-12 md:h-16 lg:h-32 xl:h-40 2xl:h-[13.8125rem] absolute top-4 left-6 md:top-4 md:left-10 lg:top-8 lg:left-16 xl:top-16 xl:left-24 2xl:top-[3.45rem] 2xl:left-[6rem] z-10"
             />
+
             {/* Company Slogan */}
-            <h2 className="text-lg md:text-xl lg:text-2xl xl:text-3xl 2xl:text-5xl font-montserrat font-bold leading-relaxed text-white uppercase relative z-20 w-full md:w-9/12 lg:w-8/12 xl:w-3/5 2xl:w-7/12">
+            <p className="text-lg md:text-xl lg:text-2xl xl:text-2xl 2xl:text-5xl/[4rem] font-montserrat font-bold text-white uppercase w-full md:w-9/12 lg:w-8/12 xl:w-3/5 2xl:w-7/12">
                 our mission is to enlighten, entertain and empower current and
                 future leaders around the world.
-            </h2>
+            </p>
+
             {/* Avater Image */}
-            <div className="md:h-[calc(100%_+_100px)] md:w-3/12 lg:w-4/12 xl:w-2/5 relative md:absolute -bottom-12 md:bottom-0 md:right-12 md:z-[5]">
+            <div className="h-40 md:h-[calc(100%_+_100px)] md:w-3/12 lg:w-4/12 xl:w-2/5 relative md:absolute -bottom-12 md:bottom-0 md:right-12 md:z-[5]">
                 <img
                     src={avater}
                     alt="avater"
