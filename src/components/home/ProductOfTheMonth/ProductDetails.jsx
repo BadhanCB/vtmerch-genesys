0aa00d20c1a85cd6f6e76de7d5c9817f3de1b005
BadhanCB <badhancsesastc112@gmail.com>
2024-09-19 15:06:04 +0600

    implemented intersection observer to add animation in product of the month section

--- src/components/home/ProductOfTheMonth/ProductDetails.jsx
+++ src/components/home/ProductOfTheMonth/ProductDetails.jsx
@@ -1,9 +1,35 @@
+import { useEffect, useRef, useState } from "react";
 import ProductInfo from "./ProductInfo";
 import ProductMedia from "./ProductMedia";
 
 const ProductDetails = () => {
+    const productDetailsRef = useRef(null);
+    const [isIntersecting, setIsIntersecting] = useState(false);
+
+    useEffect(() => {
+        const productDetailsObserver = new IntersectionObserver(
+            (entries) => {
+                setIsIntersecting(entries[0].isIntersecting);
+
+                if (entries[0].isIntersecting) {
+                    productDetailsObserver.unobserve(entries[0].target);
+                }
+            },
+            { threshold: 0.5 }
+        );
+
+        productDetailsObserver.observe(productDetailsRef.current);
+    }, []);
+
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12">
+        <div
+            ref={productDetailsRef}
+            className={`grid grid-cols-1 lg:grid-cols-2 gap-12 transition-all duration-700 ${
+                isIntersecting
+                    ? "translate-y-0 opacity-100"
+                    : "translate-y-12 opacity-0"
+            }`}
+        >
             <ProductMedia />
             <ProductInfo />
         </div>
